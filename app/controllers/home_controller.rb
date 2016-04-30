1e42fb34070d95f869e588e8ca5cfdbcfbf2c4d4
Tokirin <tokirin1117@gmail.com>
2016-04-30 16:56:01 +0900

    개인정보 처리방침 추가

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def privacy_security
+    @title = "개인정보 처리방침"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  get '/privacy_security', to: 'home#privacy_security'
   mount API => '/api/client'
 end
